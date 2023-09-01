8d813124cdcae74b62bcd8b6dc47d4034d6277a7
Arifice <arifice13@gmail.com>
2023-09-02 05:00:47 +0600

    added

---
+++ test.js
@@ -0,0 +1,9 @@
+
+function time(time){
+    const h=Math.floor(time/3600);
+    const m=Math.floor((time%3600)/60);
+    const s=Math.floor((time%3600)%60);
+    return 
+}
+const t=235887;
+console.log(time(t));
